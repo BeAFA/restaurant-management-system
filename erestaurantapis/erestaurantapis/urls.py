373e81f36bef75898a697a6cb04222a1037507ec
BeAFA <pt281973555k@gmail.com>
2026-05-05 17:48:16 +0700

    update db

--- erestaurantapis/erestaurantapis/urls.py
+++ erestaurantapis/erestaurantapis/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('order/', include('order.urls')),
+    path('admin/', admin.site.urls)
 ]
