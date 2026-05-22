73afe30c7ff676e7a76ac92a39f2b68ba58e35c4
BeAFA <pt281973555k@gmail.com>
2026-05-22 13:06:58 +0700

    Update allowed hosts and database password

--- erestaurantapis/erestaurantapis/settings.py
+++ erestaurantapis/erestaurantapis/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-!++2btq48$id0+kouoe5c!z_=go(*(3u!ld+ssbei5s1!mzgd1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.160']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -105,7 +105,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'restaurantdb',
         'USER': 'root',
-        'PASSWORD': 'Apocalypse123@',
+        'PASSWORD': 'root',
         'HOST': ''
     }
 }
